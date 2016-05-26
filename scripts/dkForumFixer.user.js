820b55caede8ba82c927697d914b58c648885539
HJ Cross <github@chimpjuice.com>
2016-05-26 00:28:31 -0400

    more formatting issues

--- scripts/dkForumFixer.user.js
+++ scripts/dkForumFixer.user.js
@@ -17,7 +17,7 @@
     document.addEventListener("DOMNodeInserted", function(event) {
         console.log("-----DOMNodeInserted");
         // remove header
-        if((document.getElementById('dkheader'))){
+        if ((document.getElementById('dkheader'))) {
             console.log('--- found dkheader');
             //(elem=document.getElementById('dkheader')).parentNode.removeChild(elem);
             //(elem=document.getElementById('dkheader')).parentNode.style.visibility = "hidden";
@@ -30,7 +30,7 @@
         // TODO: want to avoid showing/loading images at all
         var images = document.getElementsByTagName('img');
         //debugger;
-        for (var n = images.length; n--> 0;) {
+        for (var n = images.length; n-- > 0;) {
             var img = images[n];
             //if (img.getAttribute("src").toLowerCase().indexOf("logo.png") !== -1){
             //img.setAttribute("src", "");
@@ -39,33 +39,34 @@
         $('#logodk img').attr("src");
     });
 
-    $(document).ready(function(){
+    $(document).ready(function() {
         var $searchbox = $('#search-box').clone();
 
         // change BG
-        document.getElementById('bodybg').style.background="#333333";
+        document.getElementById('bodybg').style.background = "#333333";
 
         // make entire page background same as main BG and add margin
-        $('body').css("background","rgb(51, 51, 51)")
-            .css("margin-top","20px")
-            .css("margin-bottom","20px");
+        $('body').css("background", "rgb(51, 51, 51)")
+            .css("margin-top", "20px")
+            .css("margin-bottom", "20px")
+            .css("min-width", "0px");
 
         // remove header
-        var elem=document.getElementById('dkheader');
+        var elem = document.getElementById('dkheader');
         if (elem) {
             elem.parentNode.removeChild(elem);
         }
 
         // remove footer & fix bottom padding
-        elem=document.getElementById('dkfooter');
+        elem = document.getElementById('dkfooter');
         if (elem) {
             elem.parentNode.removeChild(elem);
         }
-        document.getElementById('wrap').style.paddingBottom="20px";
+        document.getElementById('wrap').style.paddingBottom = "20px";
 
         // remove row icons & adjust left margin
-        $('.row .icon').css("background-image","none");
-        $('.row .icon dt').css("padding-left","10px");
+        $('.row .icon').css("background-image", "none");
+        $('.row .icon dt').css("padding-left", "10px");
 
         // remove post profile images
         $('.postprofile img').remove();
@@ -74,11 +75,12 @@
         $('#bottom').parent().hide();
 
         // add search back and style
-        $searchbox.css("margin-top","19px")
-            .css("margin-left","19px")
-            .css("margin-right","20px");
+        $searchbox.css("margin-top", "19px")
+            .css("margin-left", "19px")
+            .css("margin-right", "20px");
         $('#wrap').prepend($searchbox);
-        document.title='- dk -';
+        $('#wrap').width('900px');
+        document.title = '- dk -';
     });
 
-})(); 
+})();
