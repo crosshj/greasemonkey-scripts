3c1eff0a7fa1a83eae5daa4a482d1dd07ab10a35
HJ Cross <github@chimpjuice.com>
2016-05-26 23:05:18 -0400

    add first util

---
+++ utils/changeCSS.js
@@ -0,0 +1,10 @@
+// https://somethingididnotknow.wordpress.com/2013/07/01/change-page-styles-with-greasemonkeytampermonkey/
+function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
