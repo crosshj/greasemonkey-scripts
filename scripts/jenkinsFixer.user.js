df1621f0c580b810acc47738177555c6330b1235
HJ Cross <github@chimpjuice.com>
2016-05-26 23:07:49 -0400

    Update jenkinsFixer.user.js

--- scripts/jenkinsFixer.user.js
+++ scripts/jenkinsFixer.user.js
@@ -3,29 +3,17 @@
 // @namespace    https://github.com/crosshj/greasemonkey-scripts/
 // @updateURL    https://github.com/crosshj/greasemonkey-scripts/raw/master/scripts/jenkinsFixer.user.js
 // @downloadURL  https://github.com/crosshj/greasemonkey-scripts/raw/master/scripts/jenkinsFixer.user.js
-// @version      0.1.7
+// @version      0.1.8
 // @description  Fix some Jira issues
 // @author       HJ Cross
 // @match        http://ci.*.com*
 // @include      /^http?://ci\..*\.com.*$/
 // @run-at document-start
 // @grant        none
+// @require      https://github.com/crosshj/greasemonkey-scripts/raw/master/utils/changeCSS.js
 // ==/UserScript==
 /* jshint -W097 */
 'use strict';
-
-
-
-// https://somethingididnotknow.wordpress.com/2013/07/01/change-page-styles-with-greasemonkeytampermonkey/
-function addGlobalStyle(css) {
-    var head, style;
-    head = document.getElementsByTagName('head')[0];
-    if (!head) { return; }
-    style = document.createElement('style');
-    style.type = 'text/css';
-    style.innerHTML = css;
-    head.appendChild(style);
-}
 
 // make builds box bigger
 addGlobalStyle('form tbody:first-child td.setting-main select { height: 450px; min-width: 640px; }');
