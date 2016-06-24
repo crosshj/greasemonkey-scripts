6019560f1eb9f39b9414abd59b2d8cde1cea11b1
crosshj <github@chimpjuice.com>
2016-06-24 14:53:39 -0400

    Update peopleFluent.user.js

--- scripts/peopleFluent.user.js
+++ scripts/peopleFluent.user.js
@@ -3,14 +3,14 @@
 // @namespace    https://github.com/crosshj/greasemonkey-scripts/
 // @updateURL    https://github.com/crosshj/greasemonkey-scripts/raw/master/scripts/peopleFluent.user.js
 // @downloadURL  https://github.com/crosshj/greasemonkey-scripts/raw/master/scripts/peopleFluent.user.js
-// @version      0.0.1
+// @version      0.0.2
 // @description  fixes some issues with People Fluent site
 // @author       crosshj
 // @match        *vms.peopleclick.com/*
 // @grant        none
 // @require      http://code.jquery.com/jquery-latest.js
 // @require      https://github.com/crosshj/greasemonkey-scripts/raw/master/utils/changeCSS.js
-// @run-at       document-start
+// @run-at       document-end
 // ==/UserScript==
 
 (function() {
