4f88c577f21695beb7a0cd86ff8679a452f6117a
crosshj <github@chimpjuice.com>
2017-09-07 15:40:29 -0400

    Update youtubeFixer.user.js

--- scripts/youtubeFixer.user.js
+++ scripts/youtubeFixer.user.js
@@ -1,25 +1,31 @@
 // ==UserScript==
 // @name         Youtube Fixer
 // @namespace    https://github.com/crosshj/greasemonkey-scripts
-// @version      0.1
-// @description  fix youtube site
-// @author       HJ Cross
-// @match        *://www.youtube.com/*
-// @grant        none
-// @run-at       document-end
+// @updateURL    https://github.com/crosshj/greasemonkey-scripts/raw/master/scripts/youtubeFixer.user.js
+// @downloadURL    https://github.com/crosshj/greasemonkey-scripts/raw/master/scripts/youtubeFixer.user.js
+// @version      0.2
+// @description  try to take over the world!
+// @author       Harrison Cross
+// @include      http://www.youtube.com/*
+// @include      https://www.youtube.com/*
+// @exclude      http://www.youtube.com/embed/*
+// @exclude      https://www.youtube.com/embed/*
+// @match        http://www.youtube.com/*
+// @match        https://www.youtube.com/*
+// @grant        GM_addStyle
+// @run-at       document-start
 // ==/UserScript==
 
+(function() {
+    'use strict';
 
-function addCSSRule(sheet, selector, rules, index) {
-  if ("insertRule" in sheet) {
-    sheet.insertRule(selector + "{" + rules + "}", index);
-  } else if ("addRule" in sheet) {
-    sheet.addRule(selector, rules, index);
-  }
-}
+    GM_addStyle ( `
+       #comments, #related {
+           display: none !important;
+       }
+       .annotation.annotation-type-custom.iv-branding { 
+           display: none !important;
+       }
+   `);
 
-// I don't want to see annotations
-addCSSRule(document.styleSheets[0],
-  ".annotation.annotation-type-custom.iv-branding", 
-  "display: none !important;"
-);
+})();
